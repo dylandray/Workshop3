b8b432630ef3bb0e455f39bfa174d154965d8dfb
louis-mtyr <94978517+louis-mtyr@users.noreply.github.com>
2024-02-26 17:24:14 +0100

    ajustement app.py

--- Python_code/app.py
+++ Python_code/app.py
@@ -1,20 +1,19 @@
 from Flask import Flask, request, jsonify
-from louis_model import GenerateModel
+import h5py, pickle
 
 app = Flask(__name__)
 
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.json
-    model, mse, r2 = GenerateModel(evaluate=True)
-    prediction = model.predict(data)
-    return create_response(data, mse, r2, prediction)
+    with h5py.File('model_Louis.pkl', 'r') as f:
+        model_louis = pickle.load(f)
+    prediction = model_louis.predict(data)
+    return create_response(data, prediction)
 
-def create_response(data, mse, r2, prediction):
+def create_response(data, prediction):
     response = {
         'data': data,
-        'mse': mse,
-        'r2 score': r2,
         'prediction': prediction
     }
     return jsonify(response)
